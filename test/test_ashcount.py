4cab65814b8199c07bdcc77db6634a70715342f2
ashkan0201 <ashkan02011@gmial.com>
2023-10-05 00:56:25 +0330

    fixed

--- test/test_ashcount.py
+++ test/test_ashcount.py
@@ -7,20 +7,20 @@ sys.path.append(path)
 
 from module.ashcount import counter , py_counter
 
-# def tset_counter1():
-#     assert counter.line_counter(path="./test/t_t.txt") == 4
+def tset_counter1():
+    assert counter.line_counter(path="./Testing_Files/Temp.txt") == 4
 
-# def tset_counter2():
-#     assert counter.counting_main_lines(path="./test/t_t.txt") == 2
+def tset_counter2():
+    assert counter.counting_main_lines(path="./Testing_Files/Temp.txt") == 2
 
-# def tset_counter3():
-#     assert counter.word_count(path="./test/t_t.txt") == 11
-
-def tset_py_counter4():
-    assert py_counter.python_line_count(path="./test/p_t.py") == 8
+def tset_counter3():
+    assert counter.word_count(path="./Testing_Files/Temp.txt") == 11
 
 def tset_py_counter4():
-    assert py_counter.python_comment_count(path="./test/p_t.py") == 2
+    assert py_counter.python_line_count(path="./Testing_Files/Temp.py") == 8
 
 def tset_py_counter4():
-    assert py_counter.pure_python_code(path="./test/p_t.py") == 3
+    assert py_counter.python_comment_count(path="./Testing_Files/Temp.py") == 2
+
+def tset_py_counter4():
+    assert py_counter.pure_python_code(path="./Testing_Files/Temp.py") == 3

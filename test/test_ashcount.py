4191558b826a83fdef4e47b81bbd5dcfb9c9ad53
ashkan0201 <ashkan02011@gmial.com>
2023-10-05 00:28:58 +0330

    fixed

--- test/test_ashcount.py
+++ test/test_ashcount.py
@@ -8,19 +8,19 @@ sys.path.append(path)
 from module.ashcount import counter , py_counter
 
 def tset_counter1():
-    assert counter.line_counter(path="text_test.txt") == 4
+    assert counter.line_counter(path="./test/text_test.txt") == 4
 
 def tset_counter2():
-    assert counter.counting_main_lines(path="text_test.txt") == 2
+    assert counter.counting_main_lines(path="./test/text_test.txt") == 2
 
 def tset_counter3():
-    assert counter.word_count(path="text_test.txt") == 11
+    assert counter.word_count(path="./test/text_test.txt") == 11
 
-def test_py_counter1():
-    assert py_counter.python_line_count(path="python_test.py") == 8
+def tset_counter4():
+    assert py_counter.python_line_count(path="./test/python_test.py") == 8
 
-def test_py_counter2():
-    assert py_counter.python_comment_count(path="python_test.py") == 2
+def tset_counter5():
+    assert py_counter.python_comment_count(path="./test/python_test.py") == 2
 
-def test_py_counter3():
-    assert py_counter.pure_python_code(path="python_test.py") == 3
+def tset_counter6():
+    assert py_counter.pure_python_code(path="./test/python_test.py") == 3

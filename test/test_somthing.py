bbfa71ff31fab070ea4e6c001f1abcc6b95b8664
ashkan0201 <ashkan02011@gmail.com>
2023-10-05 01:24:39 +0330

    final test

--- test/test_somthing.py
+++ test/test_somthing.py
@@ -16,11 +16,11 @@ def test_counter2():
 def test_counter3():
     assert counter.word_count(path="./Testing_Files/Temp.txt") == 11
 
-def test_py_counter4():
+def test_counter4():
     assert py_counter.python_line_count(path="./Testing_Files/Temp.py") == 8
 
-def test_py_counter4():
+def test_counter5():
     assert py_counter.python_comment_count(path="./Testing_Files/Temp.py") == 2
 
-def test_py_counter4():
+def test_counter6():
     assert py_counter.pure_python_code(path="./Testing_Files/Temp.py") == 3
